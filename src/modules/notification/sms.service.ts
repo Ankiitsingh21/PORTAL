c2ec0cc1a4712cbd0b343d38332bad1c242dff91
Ankit Singh <ankiitsingh21@gmail.com>
2026-06-25 17:25:27 +0530

    added the sms otp service too

--- src/modules/notification/sms.service.ts
+++ src/modules/notification/sms.service.ts
@@ -2,34 +2,24 @@ import axios from "axios";
 
 export const sendSms = async (phone: string, otp: string) => {
   try {
-    // Development mode (without MSG91)
     if (!process.env.MSG91_API_KEY) {
       console.log(`[DEV SMS] To: ${phone} | OTP: ${otp}`);
       return;
     }
 
-    const response = await axios.post(
-      "https://api.msg91.com/api/v5/flow/",
-      {
-        flow_id: process.env.MSG91_FLOW_ID,
-        sender: process.env.MSG91_SENDER_ID,
-
-        mobiles: `91${phone}`,
-
+    const response = await axios.get("https://api.msg91.com/api/v5/otp", {
+      params: {
+        authkey: process.env.MSG91_API_KEY,
+        mobile: `91${phone}`,
         otp: otp,
+        sender: process.env.MSG91_SENDER_ID,
+        otp_template_id: process.env.MSG91_TEMPLATE_ID,
       },
-      {
-        headers: {
-          authkey: process.env.MSG91_API_KEY,
-          "Content-Type": "application/json",
-        },
-      },
-    );
+    });
 
     console.log(`[SMS SENT] OTP sent to ${phone}`, response.data);
   } catch (error: any) {
     console.error("[SMS ERROR]", error.response?.data || error.message);
-
     throw new Error("Failed to send SMS");
   }
 };
